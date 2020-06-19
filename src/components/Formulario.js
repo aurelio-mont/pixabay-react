caa05e1e3b679fed5b8ed1b35403ce8830741248
aurelio-mont <aurelio.mont25@gmail.com>
2020-06-19 13:43:41 -0500

    Validando Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Error from './Error';
 
 const Formulario = () => {
+    const [termino, setTermino] = useState('');
+    const [error, setError] = useState(false);
+
+    const buscarImagenes = (e) =>  {
+        e.preventDefault();
+        // Validar
+        if (termino.trim() === ''){
+            setError(true);
+            return;
+        }
+        setError(false);
+        // Enviar el termino hacia el componente pricipal
+
+    }
     return ( 
-        <form>
+        <form
+            onSubmit={buscarImagenes}
+        >
             <div className="row">
                 <div className="form-grouip col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Busca una imagen, ejemplo: comida o café"
+                        onChange={e => setTermino(e.target.value)}
                     />
                 </div>
                 <div className="form-grouip col-md-4">
@@ -19,6 +37,7 @@ const Formulario = () => {
                     />
                 </div>
             </div>
+            { error ? <Error mensaje="Introduce un térmio de busqueda" /> : null}
         </form>
      );
 }
